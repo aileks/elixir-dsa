953f030b6d8001a9dbe73c8854b315886024dd23
Aaliyah Harper <aeverly14@pm.me>
2025-02-13 13:45:08 -0500

    added graph problem

---
+++ lib/graphs.ex
@@ -0,0 +1,7 @@
+defmodule GraphProblems do
+  # Check if path exists between two nodes in directed graph (DFS/BFS)
+  @spec has_path?(map, any, any) :: boolean
+  def has_path?(_graph, _start, _target) do
+    # Implement path existence check
+  end
+end
